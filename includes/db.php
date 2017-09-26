a0aee9adf004f91eed1467f78b53919f01256dde
Diego Camacho <diegocamacho2.0@gmail.com>
2017-09-26 18:23:53 -0500

    Modificacion de DB

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,16 @@
 <?php
 //error_reporting(0);
-/* Conexión en entorno local */
+/* Conexión en entorno local 
 $servidor="localhost";
 $usuario="root";
 $clave="root";
-$base="dentisxa2";
+$base="dentisxa2";*/
 
 /* Conexión en producción */
-/*
-$servidor="localhost";
-$usuario="d5j1g6l6_diego";
+$servidor="epicmedia.pro";
+$usuario="epic_diego";
 $clave="camacho";
-$base="d5j1g6l6_dentista";*/
+$base="epic_dentisxa";
 //exit("ESTAMOS ACTUALIZANDO SU SITEMA, ¡VOLVEREMOS PRONTO!");
 $conexion = @mysql_connect ($servidor,$usuario,$clave) or die ("Ocurrió un error al conectarse.");
 @mysql_select_db($base) or die ("No BD ");
